60cf802492af06bad933b935c97d96dd8f88ab35
Carlos Quiroga <cquiro@gmail.com>
2018-04-26 21:20:27 -0300

    Add more user specs.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,5 +6,17 @@ RSpec.describe User, type: :model do
       user = build(:user)
       expect(user).to be_valid
     end
+
+    it 'is invalid without an email' do
+      user = build(:user, email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include 'can\'t be blank'
+    end
+
+    it 'is invalid without an image' do
+      user = build(:user, image: nil)
+      user.valid?
+      expect(user.errors[:image]).to include 'can\'t be blank'
+    end
   end
 end
