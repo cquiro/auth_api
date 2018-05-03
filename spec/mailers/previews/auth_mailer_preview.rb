b7bbea628dde4c03770c6926db7f1647c7ecff88
Carlos Quiroga <cquiro@gmail.com>
2018-05-03 12:46:24 -0300

    Modify email template.

--- spec/mailers/previews/auth_mailer_preview.rb
+++ spec/mailers/previews/auth_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/auth_mailer
 class AuthMailerPreview < ActionMailer::Preview
-
+  def auth_attempt_notification
+    AuthMailer.auth_attempt('email@example.com', 'mobile')
+  end
 end
