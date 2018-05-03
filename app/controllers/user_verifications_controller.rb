76990c49db8788714e18b83c8961de9212519afb
Carlos Quiroga <cquiro@gmail.com>
2018-05-03 12:12:14 -0300

    Add user-agent info

--- app/controllers/user_verifications_controller.rb
+++ app/controllers/user_verifications_controller.rb
@@ -1,7 +1,7 @@
 class UserVerificationsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
-    AuthMailer.auth_attempt(params[:email], 'device').deliver_later
+    AuthMailer.auth_attempt(params[:email], request.user_agent).deliver_later
 
     if user && user.image == params[:image]
       head :ok
